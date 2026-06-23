89f1ece558c21079f35228da716fada1556c792a
wuyifancode <weixi414074@163.com>
2026-06-23 21:17:02 +0800

    feat(ui): switch root entry to OptimizedUiApp

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import OptimizedUiApp from "./OptimizedUiApp";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <OptimizedUiApp />
     </ErrorBoundary>
   </React.StrictMode>
 );
